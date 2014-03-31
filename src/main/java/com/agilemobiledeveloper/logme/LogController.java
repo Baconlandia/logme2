3acd17ef7f74bc289b23077f2f1ff4b6a400b0d8
Tim Spann <tim@agilemobiledeveloper.com>
2014-03-31 08:46:17 -0400

    'initial'

--- src/main/java/com/agilemobiledeveloper/logme/LogController.java
+++ src/main/java/com/agilemobiledeveloper/logme/LogController.java
@@ -1,26 +1,22 @@
 package com.agilemobiledeveloper.logme;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.util.Assert;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
--- src/main/java/com/agilemobiledeveloper/logme/LogDataService.java
+++ src/main/java/com/agilemobiledeveloper/logme/LogDataService.java
@@ -3,10 +3,7 @@ package com.agilemobiledeveloper.logme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,19 +21,13 @@ public class LogDataService {
 		logger.info("Log in ds=" + newLog.toString());
 		
 		try {
-		//	ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfiguration.class);
-			
-//			MongoOperations  mongoOperations = (MongoOperations) ctx.getBean("mongoTemplate"); // mongoConfiguration.mongoTemplate();
-	
 			MongoOperations  mongoOperations = mongoConfiguration.mongoTemplate();
 			mongoOperations.insert(newLog);
 			
 			logger.info("Saved");
 		}
 		catch(Exception x) {
-			x.printStackTrace();
 			logger.error(x.getLocalizedMessage());
 		}
-
 	}
 }
