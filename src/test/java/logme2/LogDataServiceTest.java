1d14342db14e630379d7173edb2b8b015fc8b2f5
Tim Spann <tim@agilemobiledeveloper.com>
2014-03-31 09:07:24 -0400

    Testing

---
+++ src/test/java/logme2/LogDataServiceTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package logme2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Timothy
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MongoConfiguration.class})
+public class LogDataServiceTest {
+
+	/**
+	 * Test method for {@link com.agilemobiledeveloper.logme.LogDataService#addLogMessage(com.agilemobiledeveloper.logme.LogEntry)}.
+	 */
+	@Test
+	public final void testAddLogMessage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
