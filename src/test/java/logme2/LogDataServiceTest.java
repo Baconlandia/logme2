be40002f90acb05e30f2e66718f3c9444b24a838
Tim Spann <tim@agilemobiledeveloper.com>
2014-03-31 09:17:56 -0400

    added junit

--- src/test/java/logme2/LogDataServiceTest.java
+++ src/test/java/logme2/LogDataServiceTest.java
@@ -6,6 +6,14 @@ package logme2;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.agilemobiledeveloper.logme.LogDataService;
+import com.agilemobiledeveloper.logme.LogEntry;
+import com.agilemobiledeveloper.logme.MongoConfiguration;
 
 /**
  * @author Timothy
@@ -15,12 +23,24 @@ import org.junit.Test;
 @ContextConfiguration(classes = {MongoConfiguration.class})
 public class LogDataServiceTest {
 
+	@Autowired
+	LogDataService logDataService;
+	
+	/**
+	 * Test method for {@link com.agilemobiledeveloper.logme.LogDataService#addLogMessage(com.agilemobiledeveloper.logme.LogEntry)}.
+	 */
+	@Test
+	public final void testAddEmptyLogMessage() {
+		LogEntry newLog = null;
+		logDataService.addLogMessage(newLog);
+	}
+
 	/**
 	 * Test method for {@link com.agilemobiledeveloper.logme.LogDataService#addLogMessage(com.agilemobiledeveloper.logme.LogEntry)}.
 	 */
 	@Test
 	public final void testAddLogMessage() {
-		fail("Not yet implemented"); // TODO
+		LogEntry newLog = new LogEntry("JUnit", "TEST", "TEST2");
+		logDataService.addLogMessage(newLog);
 	}
-
 }
